e044759bacbc0e43d65b85d168b5c92426c5d305
Nikita Butov <f1owly@172.31.224.145-vpn.dhcp.yndx.net>
2025-06-13 17:44:04 +0300

    saving graphics

--- graphics.py
+++ graphics.py
@@ -5,9 +5,6 @@ try:
     data = pd.read_csv('time_sorts.txt', delim_whitespace=True, header=None)
     # Присваиваем колонкам имена для удобства (необязательно, но улучшает читаемость)
     data.columns = ['X', 'Y1', 'Y2', 'Y3', 'Y4']
-    
-    print("Данные успешно загружены:")
-    print(data)
 
     # Шаг 2: Создание и настройка графика
     # Создаем фигуру и оси для графика. figsize задает размер окна с графиком.
@@ -21,13 +18,15 @@ try:
     ax.plot(data['X'], data['Y4'], marker='x', linestyle=':', label='Пирамидальная сортировка')
 
     # Шаг 3: Добавление элементов оформления
-    ax.set_title('Графики зависимостей Y от X') # Заголовок графика
-    ax.set_xlabel('Координата X')               # Подпись оси X
-    ax.set_ylabel('Координата Y')               # Подпись оси Y
+    ax.set_title('Графики зависимостей времени от размера входных данных') # Заголовок графика
+    ax.set_xlabel('Размер входных данных')               # Подпись оси X
+    ax.set_ylabel('Время выполнения, ms')               # Подпись оси Y
     ax.legend()                                 # Показать легенду (описание линий)
     ax.grid(True)                               # Включить сетку для удобства чтения
     ax.set_yscale('log')
     # Шаг 4: Отображение графика
+    output_filename = 'time_graphics.png'
+    plt.savefig(output_filename, dpi=300)
     plt.show()
 
 except FileNotFoundError:
